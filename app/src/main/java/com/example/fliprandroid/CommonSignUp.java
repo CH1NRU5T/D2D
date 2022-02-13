844870bb9801151852114ac0538cd7755422e99b
ch1nru5t <chinrust@gmail.com>
2022-02-13 18:43:13 +0530

    common signup complete

--- app/src/main/java/com/example/fliprandroid/CommonSignUp.java
+++ app/src/main/java/com/example/fliprandroid/CommonSignUp.java
@@ -2,13 +2,63 @@ package com.example.fliprandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.fliprandroid.databinding.ActivityCommonSignUpBinding;
+
+import java.util.Objects;
 
 public class CommonSignUp extends AppCompatActivity {
+    private String name;
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    ActivityCommonSignUpBinding activityCommonSignUpBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_common_sign_up);
+        activityCommonSignUpBinding = ActivityCommonSignUpBinding.inflate(getLayoutInflater());
+        setContentView(activityCommonSignUpBinding.getRoot());
+
+
+        activityCommonSignUpBinding.submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent;
+                if (activityCommonSignUpBinding.dealerRadio.isChecked()) {
+                    launchActivity(new Intent(CommonSignUp.this, DealerSignUp.class));
+                } else if (activityCommonSignUpBinding.driverRadio.isChecked()) {
+                    launchActivity(new Intent(CommonSignUp.this, DriverSignUp.class));
+                } else {
+                    Toast.makeText(CommonSignUp.this, "Please select whether you are a dealer or a driver", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    private void launchActivity(Intent intent) {
+        name = Objects.requireNonNull(activityCommonSignUpBinding.name.getText()).toString();
+        userName = Objects.requireNonNull(activityCommonSignUpBinding.userName.getText()).toString();
+        email = Objects.requireNonNull(activityCommonSignUpBinding.email.getText()).toString();
+        phoneNumber = Objects.requireNonNull(activityCommonSignUpBinding.phoneNumber.getText()).toString();
+        password = Objects.requireNonNull(activityCommonSignUpBinding.password.getText()).toString();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("name", name);
+        bundle.putString("userName", userName);
+        bundle.putString("email", email);
+        bundle.putString("phoneNumber", phoneNumber);
+        bundle.putString("password", password);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/fliprandroid/DriverSignUp.java
@@ -0,0 +1,14 @@
+package com.example.fliprandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_sign_up);
+    }
+}
