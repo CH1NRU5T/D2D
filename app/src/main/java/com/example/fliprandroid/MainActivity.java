c3c493a0e1c1b0d7191821d91c6f88099eb39ae1
ch1nru5t <chinrust@gmail.com>
2022-02-13 14:58:57 +0530

    first comit

---
+++ app/src/main/java/com/example/fliprandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fliprandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dealer_sign_up);
+    }
+}
